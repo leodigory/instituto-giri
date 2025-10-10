376e3d75633776345752f72fd19f287d32d22455
leodigory <01leonardoaraujo@gmail.com>
2025-10-10 03:25:43 -0300

    Atualizar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keyboard from './components/Keyboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Teclado Virtual</h1>
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
